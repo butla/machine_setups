51fd0161607d5fb847aa1bbeb30a175cb414f001
Michal Bultrowicz <michal.bultrowicz@gmail.com>
2021-11-24 02:12:34 +0100

    conf_diff also fetches new changes from origin

--- configs/host_agnostic/.config/zsh/aliases.zsh
+++ configs/host_agnostic/.config/zsh/aliases.zsh
@@ -101,7 +101,7 @@ alias printer_print_options='lpoptions -d Brother_DCP_J772DW -l'
 alias print_pdf_black_and_white='lpr -P Brother_DCP_J772DW -o ColorModel=Gray'
 
 # shows the changes in my machine config
-alias conf_diff='(cd ~/development/machine_setups; git status && git diff)'
+alias conf_diff='(cd ~/development/machine_setups; git fetch && git status && git diff)'
 # commits and pushes the changes in my machine config
 alias conf_push='(cd ~/development/machine_setups; git commit -a && git push)'
 alias conf_edit='(cd ~/development/machine_setups; vim workstation_setup/__main__.py; conf_push)'
