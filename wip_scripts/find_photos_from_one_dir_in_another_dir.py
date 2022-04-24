cbc6678b593443207135a2869b30c27b8322f9f8
Michal Bultrowicz <michal.bultrowicz@gmail.com>
2022-04-24 20:33:51 +0200

    new wip script

---
+++ wip_scripts/find_photos_from_one_dir_in_another_dir.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+telefon_on_pc = Path('/data/zdjęcia_i_filmiki/telefon/')
+phone_photos = Path('/home/butla/note_10_camera/')
+
+telefon_on_pc_files = set(p.name for p in telefon_on_pc.iterdir())
+phone_photos_files = set(p.name for p in phone_photos.iterdir())
+
+print(len(telefon_on_pc_files))
+print(len(phone_photos_files))
+
+print(telefon_on_pc_files - phone_photos_files)
