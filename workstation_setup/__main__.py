b97fc3695b286d7288d6376aef6ec791ff206092
Michal Bultrowicz <michal.bultrowicz@gmail.com>
2022-06-19 19:34:53 +0200

    Use `pamac checkupdates` correctly.

--- workstation_setup/__main__.py
+++ workstation_setup/__main__.py
@@ -72,7 +72,7 @@ def sync_packages():
     _run_cmd(r"sudo sed -i -E 's|^.*CheckAURUpdates|CheckAURUpdates|' /etc/pamac.conf")
 
     log.info('What will get updated?')
-    _run_cmd('pamac checkupdates')
+    subprocess.run(['pamac', 'checkupdates'], check=False)
 
     log.info('Updating the package index and packages...')
     _run_cmd('sudo pamac upgrade --no-confirm')
