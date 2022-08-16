9262c77ffbf10152dd8f4aadd18426e297d28f9b
Michal Bultrowicz <michal.bultrowicz@gmail.com>
2022-08-17 00:42:09 +0200

    Draft of a photo puller for my camera (over USB).
    It might not be needed in the end.

---
+++ configs/host_agnostic/bin/photo_transfer_olympus.py
@@ -0,0 +1,6 @@
+# TODO check
+# - configs/host_agnostic/bin/ostatnia_fota weź to, tylko dodaj parametryzację
+# configs/host_agnostic/bin/phone_media_transfer.py
+
+# sudo mount /dev/disk/by-id/usb-OLYMPUS_E-P7_BJKA03472-0:0 ~/urządzenia/olympus_filesystem
+# can be automounted already?
