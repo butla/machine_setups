d7cd4daf5c6264f9dca0d0c0db3b60ea9b8ea644
Michal Bultrowicz <michal.bultrowicz@gmail.com>
2022-08-30 09:24:46 +0200

    WIP, still broken code, adding note about RPI setup.

--- os_building_scripts/setup_rpi_manjaro_headless_server.sh
+++ os_building_scripts/setup_rpi_manjaro_headless_server.sh
@@ -7,4 +7,9 @@ RPI_IP=$1
 ssh-copy-id butla@$RPI_IP
 
 # TODO maybe I should encode that with fabric or just mitogen?
-# sudo pacman-mirrors -f
+
+# Would be nice to check when was the last time this ran. Or just put in my standard mirrors file, if it's not already
+# there. Maybe a cron (anacron) that'd run this every week?
+# sudo pacman-mirrors -f 
+
+# sshd config - have one for ognisko, bl, and bh?
