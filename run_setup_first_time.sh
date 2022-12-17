299aedd634af2cf1e797b2a3fccc632949799160
Michal Bultrowicz <michal.bultrowicz@gmail.com>
2022-12-17 01:00:41 +0100

    Note about installing poetry.

--- run_setup_first_time.sh
+++ run_setup_first_time.sh
@@ -6,5 +6,6 @@ set -e
 
 echo "----- Refreshing the the package mirrors... -----"
 sudo pacman-mirrors -f
+# TODO install poetry, if it isn't there
 echo "----- Running the setup... -----"
 python3 -m machine_setup
