3e6f97a09e9dbaf58d11e672b00215eb5dee2aba
Michal Bultrowicz <michal.bultrowicz@gmail.com>
2022-12-30 14:44:36 +0100

    Bootloader setup script: notes.

---
+++ machine_setup/os_building/setup_bp_boot.sh
@@ -0,0 +1,5 @@
+# TODO mounting (if not mounted)
+# build boot, endure systemd-boot is setup and grub uninstalled
+# Use mount_bp? Make it idempotent? Use the common boot script, make that use hosts_config
+
+# Use hosts_config.yml with yq
